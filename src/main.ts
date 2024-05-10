06e4f7db7f9a819f561e4a761a474e51e3778ec1
Justar <136484541+Justar96@users.noreply.github.com>
2024-05-10 18:42:50 +0700

    0

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+
+import './style.scss'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 50;
+        curY += (tgY - curY) / 40;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
+
+let sections =  document.querySelectorAll('section');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 200;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add('show-animate');
+    }
+    else if (top <= 200) {
+      sec.classList.remove('show-animate')
+    }
+  })
+}
+
+
+
